725124bd758461950bb18f0c3d43f65e2c6adf81
eugeny.petlakh <eugeny.petlakh@softeq.com>
2019-06-18 12:22:28 +0300

    MessageType updated

--- NetKit.Chat.TransportModels/Softeq.NetKit.Chat.TransportModels/Models/CommonModels/Request/Member/GetPotentialChannelMembersRequest.cs
+++ NetKit.Chat.TransportModels/Softeq.NetKit.Chat.TransportModels/Models/CommonModels/Request/Member/GetPotentialChannelMembersRequest.cs
@@ -1,13 +1,10 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
-using System;
-
 namespace Softeq.NetKit.Chat.TransportModels.Models.CommonModels.Request.Member
 {
     public class GetPotentialChannelMembersRequest : BaseRequest
     {
-        public Guid ChannelId { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
         public string NameFilter { get; set; }
